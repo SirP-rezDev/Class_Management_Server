666992ae1de189be3b628f247b31626f9670c201
SirP-rezDev <160800081+SirP-rezDev@users.noreply.github.com>
2025-08-14 09:21:14 +0800

    Add files via upload

---
+++ class_management_server.js
@@ -0,0 +1,53 @@
+const express = require(`express`);
+const app = express();
+const PORT = 3000;
+
+// const DB_FILE = "data.json";
+
+app.use(express.json());
+
+
+let user = [
+    {
+        id:1,
+        firstName: "Kevin",
+        lastName: "Mcgaurer",
+        section: "BSIT 4A",
+        status: "p"
+    },
+    {
+        id:2,
+        firstName: "Kevina",
+        lastName: "Mcgaurers",
+        section: "BSIT 4B",
+        status: "p"
+    }
+];
+
+
+
+app.get('/user', (res, req) => {
+     const {lastName, firstName, section, status} = req.body;
+     const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
+     if (userIndex !== -1){
+        users[userIndex].status = status;
+        console.log(`updated attendance for ${lastName} ${firstName} to :${status}`);
+        res.status(200).json({message: `attendance for ${lastName} ${firstName} has been updated to ${status}`});
+     }else{
+        const newUser = {
+            id: users.length + 1,
+            lastName,
+            firstName,
+            section,
+            status
+        };
+        user.push(newUser);
+        console.log(`new user added : ${lastName} ${firstName} with status ${status}`);
+        res.status(201).json({message: `attendance for ${lastName} ${firstName} has been updated to ${status}`});
+
+     }
+});
+
+// app.listen(PORT, () => {
+//     console.log(`server was running away from http://localhost:${PORT}`);
+// });
