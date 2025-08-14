85a8cdc5143750cfadf60e93400c25ae721715a3
SirP-rezDev <160800081+SirP-rezDev@users.noreply.github.com>
2025-08-14 09:35:53 +0800

    Update class_management_server.js

--- class_management_server.js
+++ class_management_server.js
@@ -1,64 +1,63 @@
-const express = require(`express`);
+const express = require('express');
 const app = express();
-const PORT = 3000;
-
-// const DB_FILE = "data.json";
 
 app.use(express.json());
 
-
-let user = [
-    {
-        id:1,
-        firstName: "Kevin",
-        lastName: "Mcgaurer",
-        section: "BSIT 4A",
-        status: "p"
-    },
-    {
-        id:2,
-        firstName: "Kevina",
-        lastName: "Mcgaurers",
-        section: "BSIT 4B",
-        status: "p"
-    }
+let users = [
+  {
+    id: 1,
+    firstName: "Kevin",
+    lastName: "Mcgaurer",
+    section: "BSIT 4A",
+    status: "p"
+  },
+  {
+    id: 2,
+    firstName: "Kevina",
+    lastName: "Mcgaurers",
+    section: "BSIT 4B",
+    status: "p"
+  }
 ];
 
+// Update or add user attendance
+app.post('/user', (req, res) => {
+  const { lastName, firstName, section, status } = req.body;
+  const userIndex = users.findIndex(
+    user => user.firstName === firstName && user.lastName === lastName
+  );
 
-
-app.get('/user', (res, req) => {
-     const {lastName, firstName, section, status} = req.body;
-     const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
-     if (userIndex !== -1){
-        users[userIndex].status = status;
-        console.log(`updated attendance for ${lastName} ${firstName} to :${status}`);
-        res.status(200).json({message: `attendance for ${lastName} ${firstName} has been updated to ${status}`});
-     }else{
-        const newUser = {
-            id: users.length + 1,
-            lastName,
-            firstName,
-            section,
-            status
-        };
-        user.push(newUser);
-        console.log(`new user added : ${lastName} ${firstName} with status ${status}`);
-        res.status(201).json({message: `attendance for ${lastName} ${firstName} has been updated to ${status}`});
-
-     }
-    
+  if (userIndex !== -1) {
+    users[userIndex].status = status;
+    console.log(`Updated attendance for ${lastName} ${firstName} to: ${status}`);
+    res.status(200).json({
+      message: `Attendance for ${lastName} ${firstName} has been updated to ${status}`
+    });
+  } else {
+    const newUser = {
+      id: users.length + 1,
+      lastName,
+      firstName,
+      section,
+      status
+    };
+    users.push(newUser);
+    console.log(`New user added: ${lastName} ${firstName} with status ${status}`);
+    res.status(201).json({
+      message: `Attendance for ${lastName} ${firstName} has been set to ${status}`
+    });
+  }
 });
 
-app.get('/user', (res, req) => {
-    res.status(200).json(users);
+// Get all users
+app.get('/user', (req, res) => {
+  res.status(200).json(users);
 });
 
-// app.get('/user', (res, req) => {
-//     res.send(`server was running away from http://localhost:${PORT}`);
-// });
-
-app.listen(PORT, () => {
-    console.log(`server was running away from http://localhost:${PORT}`);
+// Root route
+app.get('/', (req, res) => {
+  res.send(`Server is running.`);
 });
 
+// For Vercel: export the app (no app.listen)
 module.exports = app;
